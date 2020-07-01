cc6e1e30b2f633a39f3405d4a8560f94f2c3d649
Nirmal <nirmalpandey048@gmail.com>
2020-07-01 10:30:45 +0545

    Added Unit attribute to entity Product

--- products/models.py
+++ products/models.py
@@ -10,13 +10,28 @@ class Product(models.Model):
                  ('indoor service','indoor service'),
                  ('outdoor service','outdoor service')
               )
+   
+    PRICE_UNITS_CHOICES = (
+     #   The first element in each tuple is the actual value to be set on the model, and the second element is the human-readable name.
+        ('UNIT', 'Choose Product Unit'),
+        ('Kg', 'kg'),
+        ('L','litre'),
+        ('Pcs','pcs'),
+        ('m2', 'm2'),
+        ('m3', 'm3'),
+        ('ml', 'ml'),
+      
+     
+    )
     name=models.CharField(max_length=50,null=True)
     price=models.DecimalField(max_digits=20,decimal_places=2)
     quantity = models.IntegerField(blank=False,default=1)
+    unit = models.CharField(max_length= 50,choices = PRICE_UNITS_CHOICES,default='')
     # slug = models.SlugField(max_length=80,default=False)
     category=models.CharField(max_length=200,null=True,choices=category)
     description=models.CharField(max_length=50,null=True)
     created_at=models.DateTimeField(auto_now=True,null=True)
+   
     
 
 
