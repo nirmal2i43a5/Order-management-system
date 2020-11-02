19f75614ec917fbb11a63b1b5d09d4f030a284bc
Nirmal <nirmalpandey048@gmail.com>
2020-11-02 11:17:32 +0545

    Updated

--- rms/settings.py
+++ rms/settings.py
@@ -98,19 +98,19 @@ Using below database is for development and postgres is for production (for post
 """
 
 
-DATABASES = {
-    'default': {
+# DATABASES = {
+#     'default': {
         
         
-        'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME':'supermarket',
-        'HOST':'127.0.0.1',
-        'USER':config('db_user'),
-        'PASSWORD':config('db_password')
+#         'ENGINE': 'django.db.backends.mysql',
+#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'NAME':'supermarket',
+#         'HOST':'127.0.0.1',
+#         'USER':config('db_user'),
+#         'PASSWORD':config('db_password')
         
-    }
-}
+#     }
+# }
 
 
 
