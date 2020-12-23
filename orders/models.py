9c2d635c8ab8961f0fb81069377ab03585c09d0a
Nirmal <nirmalpandey048@gmail.com>
2020-12-23 23:20:10 +0545

    change timefield

--- orders/models.py
+++ orders/models.py
@@ -22,7 +22,7 @@ class Order(models.Model):
     Similarly if Product is deleted then order set to Null value'''
     
     # created_at=models.DateTimeField(max_length=50,null=True,auto_now=True)
-    created_at=models.DateField(auto_now=True)#chart implement with this field
+    created_at=models.DateField()#chart implement with this field
     status=models.CharField(max_length=100,null=True,choices=status)
     quantity = models.IntegerField(default=1,blank=False)
     # total_price = models.DecimalField(max_digits=10,decimal_places=2,default=0)
